dd488271e4e095deb649cd33f41b623c53f15127
Mears <7mears@gmail.com>
2014-04-18 16:36:36 -0600

    mostly responsive stuff

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*== Smooth Scroll ==============================*/
+/*== Smooth Scrolling ==============================*/
 	$(function() {
 	  $('a[href*=#]:not([href=#])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
