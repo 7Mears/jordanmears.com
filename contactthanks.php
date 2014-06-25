f554af621c3205c4fc14d79c148b93cf49ae8f14
Jordan Mears <7mears@gmail.com>
2014-06-25 15:52:50 -0600

    Few style changes, added basic responsive, added contact form

---
+++ contactthanks.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Jordan Mears</title>
+<meta name="description" content="Freelance web designer">
+<meta name="author" content="Jordan Mears">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!-- CSS  -->
+<link rel="stylesheet" href="css/style.css"> 
+<!-- Font  -->
+<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,700|Roboto:100,200,300' rel='stylesheet' type='text/css'>
+</head>
+<body>  
+<!-- Header  -->
+    <header>    
+        <div class="wrap">      
+                <nav>
+                    <ul>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+        </div>
+    </header>
+<!--Home  -->
+    <section id="home">
+        <div class="wrap">
+            <div id="homeContent">
+                <h3>Your message has been sent!</h3>
+                <a href="#contact" class="button">Go back</a>
+            </div>
+        </div>
+    </section>
+
+<!-- JS  -->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+    
+</body>
+</html>
--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
-/*== Smooth Scrolling ==============================*/
+    /*
+    * Replace all SVG images with inline SVG
+    */
+        jQuery('img.svg').each(function(){
+            var $img = jQuery(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
 
-$(function() {
-    
-        //set home active on load
-    $("a[href='#home']").addClass("currSection active");
+            jQuery.get(imgURL, function(data) {
+                // Get the SVG tag, ignore the rest
+                var $svg = jQuery(data).find('svg');
 
-    $('a[href*=#]:not([href=#])').click(function() {
-	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                // Add replaced image's ID to the new SVG
+                if(typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image's classes to the new SVG
+                if(typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass+' replaced-svg');
+                }
 
-	    var target = $(this.hash);
-	    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-	    if (target.length) {
-		$('html,body').animate({
-		    scrollTop: target.offset().top
-		}, 1000);
-		return false;
-	    }
-	}
-    });
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg = $svg.removeAttr('xmlns:a');
+
+                // Replace image with new SVG
+                $img.replaceWith($svg);
+
+            }, 'xml');
+
+        });
